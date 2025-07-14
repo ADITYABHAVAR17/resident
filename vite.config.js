e8e3eb160abf6e35c181e08c623a9359afd307a4
ADITYABHAVAR17 <adityabhavar17@gmail.com>
2025-07-14 22:30:28 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -26,13 +26,7 @@ export default defineConfig(() => {
       viteLogger(),
       viteTsconfigPaths(),
     ],
-    server: {
-      // this ensures that the browser opens upon server start
-      open: true,
-      // this sets a default port to 3000
-      port: 8000,
-      host: "127.0.0.1",
-    },
+   
     optimizeDeps: {
       esbuildOptions: {
         loader: {
